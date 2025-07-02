ff15ac85385b8c03082315d0829f5cc4ec2a2299
agawish1 <agawish@spirawn.com>
2025-07-02 17:33:36 +0300

    Initial Commit

---
+++ TEA-Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TEA-Movies
+//
+//  Created by Alaa Gawish on 02/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
