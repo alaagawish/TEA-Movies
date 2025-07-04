ec43d04c6575da2968b9bdd0217a668d52876842
agawish1 <agawish@spirawn.com>
2025-07-04 20:14:02 +0300

    testin viewmodel with 93% coverage

--- TEA-Movies/Service/Repository/Repository.swift
+++ TEA-Movies/Service/Repository/Repository.swift
@@ -7,29 +7,36 @@
 
 import Foundation
 
-class Repository: NetworkProtocol, LocalProtocol {
-    
+class Repository: RepositoryProtocol {
+    var isConnected: Bool
+    var connectivity: ConnectivityProtocol!
     var network: NetworkProtocol!
     var localDataStore: LocalProtocol!
     init(network: NetworkProtocol!, localDataStore: LocalProtocol!) {
-        startMonitoring()
+        connectivity = Connectivity()
+        isConnected = false
+        connectivity.startMonitoring()
         self.network = network
         self.localDataStore = localDataStore
+        
     }
     func getMoviesList(sortedBy: SortTypes, releaseYear: Int, token: String, completion: @escaping (MoviesResponse?) -> Void) {
         self.network.getMoviesList(sortedBy: sortedBy, releaseYear: releaseYear, token: token, completion: completion)
     }
     func startMonitoring() {
-        Connectivity.shared.startMonitoring()
+        connectivity.startMonitoring()
     }
     func isConnectedToInternet() -> Bool {
-        return Connectivity.shared.isConnected
+        isConnected = connectivity.isConnected
+        return connectivity.isConnected
     }
     
     func add(movies: [MoviesResponseResults]) {
         localDataStore.add(movies: movies)
     }
-    
+    func waitUntilChecked(completion: @escaping (Bool) -> Void) {
+        completion(self.isConnected)
+    }
     func changeFaveOfMovie(movie: MoviesResponseResults, isFave: Bool) {
         localDataStore.changeFaveOfMovie(movie: movie, isFave: isFave)
     }
